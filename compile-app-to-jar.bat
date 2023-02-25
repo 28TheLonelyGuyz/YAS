763f33df2b18d1a6c68b61a6ab50bd67e66afe4e
LonliHH <77958586+28TheLonelyGuyz@users.noreply.github.com>
2023-02-25 16:01:55 +0800

    Update compile-app-to-jar.bat

--- compile-app-to-jar.bat
+++ compile-app-to-jar.bat
@@ -1,31 +1,32 @@
 @echo off
-
 title Compile to Jar
 goto start
 
 :start
-echo Enter filename for jar file
+echo Enter name for jar file
 set /p filename=
 goto compile
 
 :compile
-set /a rand0=%random%
-set /a rand1=%random%
-set /a rand2=%random%
-set /a id=(%rand0%)*(%rand1%)/(%rand2%)
+set /a id="%random%"
 set manifest_filename="%id%-manifest"
 
+:: Packing assets
 cd assets_packer
 echo Packing assets...
 java -jar "yas-assets-packer.jar" ../assets ../app_bin/assistant.pak excluded_files.txt
 cd ..
 
-rem Creating the manifest file
+:: Creating manifest file
 echo Main-Class: lonli.Main> %manifest_filename%
 echo.>> %manifest_filename%
 
-jar -cvfm "app_bin/%filename%" %manifest_filename% lonli/*.class lonli/*.txt lonli/lua/*.class lonli/lua/*.txt lonli/modsupport/*.class lonli/modsupport/*.txt copyfiles/*.class copyfiles/*.txt lonliengine/*.class lonliengine/*.txt org/luaj/* *.class org/json/*
+:: Packing and compiling all class files
+cd src
+jar -cvfm "../app_bin/%filename%.jar" "../%manifest_filename%" lonli/*.class lonli/*.txt lonli/lua/*.class lonli/lua/*.txt lonli/modsupport/*.class lonli/modsupport/*.txt copyfiles/*.class copyfiles/*.txt lonliengine/*.class lonliengine/*.txt org/luaj/* org/json/* *.class
 
+:: Finishing stuff
+cd ..
 del %manifest_filename%
 echo.
 echo.
